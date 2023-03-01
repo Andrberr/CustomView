290d1527ffffe53b77d0827f27c29dc49ecb20f3
BABASTAS728 <Aberezkinn954@gmail.com>
2023-03-01 21:52:50 +0300

    animation

--- app/src/main/java/com/example/customview/MainActivity.kt
+++ app/src/main/java/com/example/customview/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.example.customview
 
-import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.animation.AnimationUtils
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import com.example.customview.operations_counter.OperationsCounterView
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +17,34 @@ class MainActivity : AppCompatActivity() {
 
         val counter = findViewById<OperationsCounterView>(R.id.customView)
         val operationsButton = findViewById<Button>(R.id.operationsButton)
+//        val animator = ValueAnimator.ofFloat(1f, 0f)
+//        animator.addUpdateListener {animation ->
+//            counter.alpha = (animation.animatedValue as Float)
+//        }
+//        animator.duration = 5000
+//        val animator = ObjectAnimator.ofFloat(counter, View.ALPHA, 1f, 0f).apply {
+//            duration = 20000
+//        }
+//
+//        val animator2 = ObjectAnimator.ofFloat(counter, View.ROTATION_Y, 0f, 360f).apply {
+//            duration = 10000
+//        }
+//
+//        val animator3 = ObjectAnimator.ofFloat(counter, View.ROTATION_X, 0f, 360f).apply {
+//            duration = 10000
+//        }
+//
+//        val animator4 = ObjectAnimator.ofFloat(counter, View.ROTATION, 0f, 360f).apply {
+//            duration = 10000
+//        }
+//
+//        val buttonAnimator = ObjectAnimator.ofFloat(operationsButton, View.ROTATION, 0f, 360f).apply {
+//            duration = 10000
+//        }
+
+        var rotate = 0f
+        val set = AnimationUtils.loadAnimation(this, R.anim.animation)
+        set.fillAfter = true
         operationsButton.setOnClickListener {
             if (mode) {
                 operationsButton.text = getString(R.string.decrease_text)
@@ -26,6 +55,17 @@ class MainActivity : AppCompatActivity() {
                 mode = true
             }
             counter.setMode()
+
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+         //   counter.startAnimation(set)
+           // counter.animate().rotationXBy(rotate).rotationX(rotate + 200).start()
+           // rotate+=180f
+//            animator.start()
+//            animator2.start()
+//            animator3.start()
+//            animator4.start()
+//            buttonAnimator.start()
         }
     }
 }
